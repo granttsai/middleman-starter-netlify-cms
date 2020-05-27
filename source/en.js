06e065c87d0bde66c55b9f9f24280984929ed13e
granttsai <grantjlt@gmail.com>
2020-05-27 19:13:22 +0800

    Update en.js

--- source/en.js
+++ source/en.js
@@ -1,1 +1,1 @@
-document.write("<a href=https://blog.msnking.com/p/sitemap.html><img src=https://1.bp.blogspot.com/-6pYg9iV25UM/Xs5J7m_shTI/AAAAAAAAGS0/0RfGL_npLmoc3wHE4tTBaNwOfv_Rt_TrwCLcBGAsYHQ/s320/j.png></a>");
+document.write("<a href=https://eng.msnking.com/2015/09/freetv.html><img src=https://1.bp.blogspot.com/-6pYg9iV25UM/Xs5J7m_shTI/AAAAAAAAGS0/0RfGL_npLmoc3wHE4tTBaNwOfv_Rt_TrwCLcBGAsYHQ/s640/j.png></a>");
